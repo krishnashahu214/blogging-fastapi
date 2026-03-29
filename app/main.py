ba897bb0bb81bd7d8672235d08108640fc591fa9
krishnashahu214 <krishnashahu214@gmail.com>
2026-03-29 20:04:32 +0530

    v 0.0.0.1

---
+++ app/main.py
@@ -0,0 +1,192 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional, List
+from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from . import models, schemas
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+
+
+models.Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+origins = ["https://www.google.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+class Post(BaseModel):
+    title : str
+    content : str
+    published : bool = True
+
+
+while True:
+    try:
+        conn = psycopg2.connect(host = 'localhost', database = 'fastapi', user= 'postgres',
+                                password = 'Krishna@1234', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection waas successfull!")
+        break
+    except Exception as error:
+        print("Connecting to Database failed")
+        print("Error: ",error)
+        time.sleep(2)
+
+
+
+my_posts = [{"title":"title 1","content":"content 1","id":1},{"title":"title 2","content":"content 2","id":2},]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+    
+
+def find_index_posts(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
+@app.get("/")
+def root():
+    return{"message": "Welcome to my APIiiiii"}
+
+# @app.get("/sqlalchemy")
+# def test_posts(db: Session = Depends(get_db)):
+
+#     posts = db.query(models.Post).all()
+#     return {"data":posts}
+
+
+
+# @app.get("/posts")
+# def get_posts():
+#     cursor.execute("""SELECT * FROM posts """)
+#     posts =cursor.fetchall()
+#     return {"data":posts}
+@app.get("/posts", response_model=list[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+
+
+
+
+# @app.post("/posts", status_code= status.HTTP_201_CREATED)
+# def create_posts(post : Post):
+#     cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+#                    (post.title, post.content, post.published))
+#     new_post = cursor.fetchone()
+#     conn.commit()
+#     return {"data":new_post}
+@app.post("/posts", status_code= status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post : schemas.PostCreate, db: Session = Depends(get_db)):
+    
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return  new_post
+
+
+
+# @app.get("/posts/{id}")
+# def get_post(id: int, response: Response):
+
+#     cursor.execute(""" SELECT * FROM posts WHERE id = %s""", (str(id)))
+#     post = cursor.fetchone()
+
+#     if not post:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail=f"post with id {id} was not found")
+
+#         # response.status_code = status.HTTP_404_NOT_FOUND
+#         # return{'message':f"post with id {id} not found"}
+
+#     return{"post_detail": post}
+@app.get("/posts/{id}", response_model= schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return{'message':f"post with id {id} not found"}
+
+    return post
+
+
+
+
+
+
+
+
+# @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+# def delete_post(id: int):
+    # deleting post 
+    # find the index in the array that has required ID
+    # my_posts.pop(index)
+
+    # cursor.execute("""DELETE FROM posts where id = %s RETURNING * """,(str(id)))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    # if deleted_post == None:
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+    #                         detail=f"post with id {id} was not found")
+    # return Response(status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+    post.delete(synchronize_session=False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+# @app.put("/posts/{id}")
+# def update_post(id: int, post: Post):
+
+#     cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
+#                    (post.title,post.content,post.published,(str(id))))
+#     updated_post = cursor.fetchone()
+#     conn.commit()
+
+#     if updated_post == None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail=f"post with id {id} was not found")
+    
+    
+#     return {"data": updated_post}
+@app.put("/posts/{id}", response_model= schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return post_query.first()
---
+++ app/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+# class Post(BaseModel):
+#     title : str
+#     content : str
+#     published : bool = True
+
+# class CreatePost(BaseModel):
+#     title : str
+#     content : str
+#     published : bool = True
+
+# class UpdatePost(BaseModel):
+#     title : str
+#     content : str
+#     published : bool
+
+class PostBase(BaseModel):
+    title : str
+    content : str
+    published : bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id : int
+    created_at : datetime
+
+    class Config:
+        from_attributes = True
+
